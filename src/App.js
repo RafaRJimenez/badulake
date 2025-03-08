f8aa06032c1f3b027528a458dc2d4f67a1589630
Rafa <rrl89@hotmail.com>
2025-03-08 18:53:31 +0100

    setting taildwind

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <div className="bg-green-500 text-white p-4">
+      ¡Probando Tailwind!
+    </div>
       </header>
     </div>
   );
