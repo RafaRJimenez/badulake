6924bda721e88e9d5708d8c771977ea2bb2ce1b5
Rafa <rrl89@hotmail.com>
2025-03-22 18:52:55 +0100

    getting and showing products from api

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -152,23 +152,22 @@ const MainPage = ({products, fetching, fetchProducts}) => {
             </div>
           </div>
 
-          {/* Cuadrícula de productos (columna derecha) */}
-          {/* <div className="col-span-1 md:col-span-3">
+          Cuadrícula de productos (columna derecha) 
+           <div className="col-span-1 md:col-span-3">
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
-              {products.map((product) => (
+            {products && products.map((product) => (
                 <div key={product.id} className="relative">
-             
                   <img
-                    src={product.image}
+                    src={product.images[0]}
                     alt={product.title}
                     className="w-full h-auto object-cover mb-2"
                   />
                   <h3 className="text-sm font-semibold text-gray-800">{product.title}</h3>
-                  <p className="text-sm text-gray-600">{product.price}</p>
+                  <p className="text-sm text-gray-600">{product.price}€</p>
                 </div>
-              ))}
+            ))}
             </div>
-          </div> */}
+          </div>
         </div>
 
         {/* Paginación */}
--- src/components/containers/MainPageContainer.jsx
+++ src/components/containers/MainPageContainer.jsx
@@ -5,8 +5,9 @@ import { fetchProducts } from '../../redux/productActions'
 import MainPage from '../MainPage'
 
 const mapStateToProps = (state) => {
+    console.log(state);
     return {
-        products: state.products,
+        products: state.products.products.products,
         fetching: state.fetching
     }
 }
